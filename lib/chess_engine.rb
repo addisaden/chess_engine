9a005edd660846adb03d44466774697f34a14326
Adrian E <addisaden@gmail.com>
2013-09-24 16:18:14 +0200

    added pry and a basic unfinished game class

--- lib/chess_engine.rb
+++ lib/chess_engine.rb
@@ -1,5 +1,23 @@
 require "chess_engine/version"
 
 module ChessEngine
-  # Your code goes here...
+  class Game
+    SHORT = {
+      :king => :k,
+      :queen => :q,
+      :rook => :r,
+      :bishop => :b,
+      :knight => :n,
+      :pawn => :p
+    }
+    def initialize(clear=false)
+      @pieces = {}
+      [:black, :white].each do |color|
+        @pieces[color] = {}
+        SHORT.keys.each do |piece|
+          @pieces[color][piece] = []
+        end
+      end
+    end
+  end
 end
