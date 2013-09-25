98d1b749c87649c717c510862269fc3d1dae2aa6
Adrian E <addisaden@gmail.com>
2013-09-25 11:12:55 +0200

    readbility of ChessEngine::Position#positions and added a placeholder for #relative_position

--- lib/chess_engine/position.rb
+++ lib/chess_engine/position.rb
@@ -2,20 +2,30 @@ require "chess_engine/constants"
 
 module ChessEngine
   module Position
+
+
+    def relative_position(field_symbol, relative_y, relative_x)
+    end
+
     def positions
       result = {}
+
       @pieces.each do |color, pieces|
         pieces.each do |name, piece_positions|
-          piece_positions.each do |position|
-            if position.last =~ /^[a-h][1-8]$/ then
-              last_position = position.last
-              raise "Position is placed 2 times" if result.has_key?(last_position)
-              result[last_position] = { color: color, piece: name }
+          piece_positions.each do |position_history|
+
+            # threw loops get: color, name, current_position
+            if (current_position = position_history.last) =~ /^[a-h][1-8]$/ then
+              raise "Position is placed 2 times" if result.has_key?(current_position)
+              result[current_position] = { color: color, piece: name }
             end
+
           end
         end
       end
+
       return result
     end
+
   end
 end
