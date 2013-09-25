3668fa51c55cce90a236b92cf4d8d25bcd590bf1
Adrian E <addisaden@gmail.com>
2013-09-25 16:26:11 +0200

    possible_queen_moves *OPTIMIZED*

--- lib/chess_engine/possible_moves.rb
+++ lib/chess_engine/possible_moves.rb
@@ -1,39 +1,48 @@
 module ChessEngine
   module PossibleMoves
-    
-    def possible_queen_move(color=self.color)
+
+    def possible_queen_moves(color=self.color)
       # setup
+      queen_directions = [-1,0,1].product [-1,0,1]
+      queen_directions.delete [0,0]
       current_positions = self.positions
       result = []
+      
+      directory_steps_helper(queen_directions, :queen, color) do |possible_move|
+        yield_result = true
 
-      # run threw all queens (yes, somtimes there are more than just one queen)
-      @pieces[color][:queen].each do |queen_history|
-        if (current_queen_position = queen_history.last) =~ /^[a-h][1-8]$/ then
-          #shorthand
-          cqp = current_queen_position
+        if current_positions.keys.include? possible_move then
+          unless current_positions[possible_move][:color] == color then
+            result << "Qx#{ possible_move }"
+          end
+          yield_result = false
+        else
+          result << "Q#{ possible_move }"
+        end
 
-          # check the steps
-          [-1,0,1].product([-1,0,1]).each do |direction|
-            (1..7).each do |queen_step|
-              move_y = queen_step * direction[0]
-              move_x = queen_step * direction[1]
-              if(relative_queen_position = relative_position(cqp, move_y, move_x)) then
+        yield_result
+      end
 
-                # is field set with another piece?
-                if current_positions.keys.include? relative_queen_position then
-                  unless current_positions[relative_queen_position][:color] == color then
-                    result << "Qx#{ relative_queen_position }"
-                  end
-                  break
-                else
-                  result << "Q#{ relative_queen_position}"
-                end
+      return result
+    end
 
+
+    def directory_steps_helper(directions, piece, color=self.color, &block)
+      @pieces[color][piece].each do  |piece_history|
+        # is piece alive?
+        if (current_piece_position = piece_history.last) =~ /^[a-h][1-8]$/ then
+          cpp = current_piece_position
+          # then run directions.
+          directions.each do |direction|
+            (1..7).each do |piece_step|
+              move_y = piece_step * direction[0]
+              move_x = piece_step * direction[1]
+              if(relative_piece_position = relative_position(cpp, move_y, move_x)) then
+                # THIS IS THE YIELD BLOCK - IF YIELD RETURNS FALSE IT BREAKS THE INNER LOOP !!!
+                break unless block.call(relative_piece_position)
               end
             end
           end
-
-          return result
         end
       end
     end
