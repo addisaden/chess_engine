7aff535e5a0ca5d38b0a0cc0e9ed5b87999dc648
Adrian E <addisaden@gmail.com>
2013-09-25 16:48:51 +0200

    possible_queen_moves *OPTIMIZED* - threw possible_direction_moves function

--- lib/chess_engine/possible_moves.rb
+++ lib/chess_engine/possible_moves.rb
@@ -2,32 +2,36 @@ module ChessEngine
   module PossibleMoves
 
     def possible_queen_moves(color=self.color)
-      # setup
       queen_directions = [-1,0,1].product [-1,0,1]
       queen_directions.delete [0,0]
+      return possible_direction_moves(queen_directions, :queen, color)
+    end
+
+    # King, Queen, Rook, Bishop - NOT Knight, Pawn
+    def possible_direction_moves(directions, piece, color, one_step=false)
       current_positions = self.positions
       result = []
-      
-      directory_steps_helper(queen_directions, :queen, color) do |possible_move|
-        yield_result = true
+      short_name = SHORTNAME[piece]
+
+      direction_steps_helper(directions, piece, color) do |possible_move|
+        yield_result = one_step ? false : true
 
         if current_positions.keys.include? possible_move then
           unless current_positions[possible_move][:color] == color then
-            result << "Qx#{ possible_move }"
+            result << "#{ short_name }x#{ possible_move }"
           end
           yield_result = false
         else
-          result << "Q#{ possible_move }"
+          result << "#{ short_name }#{ possible_move }"
         end
 
-        yield_result
+        yield_result # THIS IS IMPORTANT
       end
 
       return result
     end
 
-
-    def directory_steps_helper(directions, piece, color=self.color, &block)
+    def direction_steps_helper(directions, piece, color=self.color, &block)
       @pieces[color][piece].each do  |piece_history|
         # is piece alive?
         if (current_piece_position = piece_history.last) =~ /^[a-h][1-8]$/ then
